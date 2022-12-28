21ea46a5e13bb59ebe4c0b195bdb29a6bac77f67
woopaca <jcw001031@gmail.com>
2022-12-28 12:59:17 +0900

    회원 레포지토리 추가

---
+++ src/main/java/com/woopaca/progether/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.woopaca.progether.repository;
+
+import com.woopaca.progether.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
