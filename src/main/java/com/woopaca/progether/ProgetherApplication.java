2d08125cb5d40cafd0956b805131730d05e69cc6
woopaca <jcw001031@gmail.com>
2022-12-28 12:48:40 +0900

    initial commit

---
+++ src/main/java/com/woopaca/progether/ProgetherApplication.java
@@ -0,0 +1,13 @@
+package com.woopaca.progether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgetherApplication.class, args);
+	}
+
+}
