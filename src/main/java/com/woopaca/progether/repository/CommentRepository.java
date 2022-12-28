0ed905bdecb5f6d16a5e70bd0710d94c37b33c30
woopaca <jcw001031@gmail.com>
2022-12-28 13:01:05 +0900

    댓글 레포지토리 추가

---
+++ src/main/java/com/woopaca/progether/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.woopaca.progether.repository;
+
+import com.woopaca.progether.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
