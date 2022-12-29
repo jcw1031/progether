b306d6a6ccda66b26100eb77ceeeba64662e1cbc
woopaca <jcw001031@gmail.com>
2022-12-29 23:15:11 +0900

    프로필 카드 추가

--- src/main/java/com/woopaca/progether/controller/ProfileController.java
+++ src/main/java/com/woopaca/progether/controller/ProfileController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BoardController {
+public class ProfileController {
 
-    @GetMapping("")
-    public String home() {
-        return "index";
+    @GetMapping("/profile")
+    public String profileMain() {
+        return "profile/profile";
     }
 }
